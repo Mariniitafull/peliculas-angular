966662d7e46642783e101b17acbad93b44c940bc
CNA06P008\Tarde <marinafullferrer@gmail.com>
2022-07-25 20:23:31 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { JuegoComponent } from './components/juego/juego.component';
+import { PeliculaAleatoriaComponent } from './components/pelicula-aleatoria/pelicula-aleatoria.component';
+import { AnfitrionesComponent } from './components/anfitriones/anfitriones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculasComponent,
+    JuegoComponent,
+    PeliculaAleatoriaComponent,
+    AnfitrionesComponent
   ],
   imports: [
     BrowserModule
